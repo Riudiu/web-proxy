749f36a11f359463d7905819436005a3770c78b4
Riudiu <lacvert13@gmail.com>
2023-05-22 21:03:36 +0900

    add socket Makefile

--- socket/host-info.c
+++ socket/host-info.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
         exit(0);
     }
 
-    /* get a list if addrinfo records */
+    /* hint 구조체 세팅 */
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
@@ -20,14 +20,19 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    /* Walk the list and display each IP address */
-    flags = NI_NUMERICHOST;  // display address string instead of domain name
+    /*
+        addrinfo 리스트 하나하나 살펴보면서 ai_addr, 즉 IP주소를 반환한다. 
+        domain은 getaddrinfo에서 받았고, 
+        해당 domain에 대응되는 많은 IP주소들이 addrinfo 리스트에 나온다.
+        각각의 IP주소를 domain으로 변환시키지 않고 그냥 출력한다.
+    */
+    flags = NI_NUMERICHOST;  // 도메인 이름을 리턴하지 않고 10진수 주소 스트링을 대신 리턴한다.
     for (p = listp; p; p = p->ai_next) {
         Getnameinfo(p->ai_addr, p->ai_addrlen, buf, MAXLINE, NULL, 0, flags);
         printf("%s\n", buf);
     }
 
-    /* clean up */
+    /* clean up - addrinfo 구조체를 free */
     Freeaddrinfo(listp);
 
     exit(0);
